140b47abd5a4d7e4e9c5c4b044951998c1d7ea39
RosaChoi <rosa.uniq@gmail.com>
2015-02-25 21:41:56 -0800

    add edit route, form and template

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('entries', function() {
     this.route('new');
+    this.route('edit', {path: '/:entry_id/edit'});
     this.route('show', {path: '/:entry_id'});
+
   });
 });
 
